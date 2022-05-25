ddd21702aa5ef516692078220afb27c634b73fc4
dion <dion_koten@yahoo.com>
2022-05-25 15:10:58 +0700

    nah cen

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -86,134 +86,229 @@
     </header>
 
     <!-- end header  -->
-    <!-- section about us  -->
-    <section class="service">
-        <div class="container">
-            <div class="row">
-              <div class="col-lg-12 float-end text-end">
-                <h1 class="service-tittle-info">Services.</h1>
-                <h3 class="service-mini-info">OUR SERVICES FOR CLIENTS</h3>
-              </div>
-            </div>
-          <div class="row test1">
-              <!-- section about us  -->
-            <div class="col-lg-3 test">
-              <div class="card1">
-		            <a href="#" class="card__link">
-                  <!-- Media -->
-                  <div class="row">
-                    <div class="card__media">
-                      <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
-                    </div>
-                  </div>
-                  <!-- Header -->
-                  <div class="row">
-                    <div class="card__header">
-                      <h3 class="card__header-title">Title of Card</h3>
-                      <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                  </div>
-                  <div class="row">
-                      <div class="card__header-icon">
-                        <svg viewbox="0 0 28 25">
-                          <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
-                        </svg>
-                      </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            
-              <!-- section about us  -->
-            <div class="col-lg-3 test">
-              <div class="card1">
-		            <a href="#" class="card__link">
-                  <!-- Media -->
-                  <div class="row">
-                    <div class="card__media">
-                      <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
-                    </div>
-                  </div>
-                  <!-- Header -->
-                  <div class="row">
-                    <div class="card__header">
-                      <h3 class="card__header-title">Title of Card</h3>
-                      <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                  </div>
-                  <div class="row">
-                      <div class="card__header-icon">
-                        <svg viewbox="0 0 28 25">
-                          <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
-                        </svg>
-                      </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            
-              <!-- section about us  -->
-            <div class="col-lg-3 test">
-              <div class="card1">
-		            <a href="#" class="card__link">
-                  <!-- Media -->
-                  <div class="row">
-                    <div class="card__media">
-                      <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
-                    </div>
-                  </div>
-                  <!-- Header -->
-                  <div class="row">
-                    <div class="card__header">
-                      <h3 class="card__header-title">Title of Card</h3>
-                      <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                  </div>
-                  <div class="row">
-                      <div class="card__header-icon">
-                        <svg viewbox="0 0 28 25">
-                          <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
-                        </svg>
-                      </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            
-              <!-- section about us  -->
-            <div class="col-lg-3 test">
-              <div class="card1">
-		            <a href="#" class="card__link">
-                  <!-- Media -->
-                  <div class="row">
-                    <div class="card__media">
-                      <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
-                    </div>
-                  </div>
-                  <!-- Header -->
-                  <div class="row">
-                    <div class="card__header">
-                      <h3 class="card__header-title">Title of Card</h3>
-                      <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                  </div>
-                  <div class="row">
-                      <div class="card__header-icon">
-                        <svg viewbox="0 0 28 25">
-                          <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
-                        </svg>
-                      </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-              
-              
-          </div>
+
+    <!-- service -->
+    <section class="page-service">
+      <div class="container">
+      <div class="row service">
+        <h1>Our Services</h1>
+      </div>
+      <div class="row service-us">
+        <div class="col-lg-6">
+          <img src="https://ibixion.com/wp-content/uploads/2020/04/Hacker-II-1.jpg" class="img-fluid">
+        </div>
+        <div class="col-lg-5">
+          <h4>You work like at home</h4>
+          <p>Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya.</p>
+          <h4>You work like at home</h4>
+          <p>Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya.</p>
+          <h4>You work like at home</h4>
+          <p>Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya.</p>
+          <h4>You work like at home</h4>
+          <p>Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya.</p>
+        </div>
+      </div>
       </div>
     </section>
-    <!-- card  -->
-    <!-- end section about us  -->
+    <!-- end service -->
+
+    <!-- section why  -->
+    <section class="page-why">
+      <div class="container">
+        <div class="row why">
+          <h1>Why Choose Us?</h1>
+        </div>
+        <div class="row why-us">
+          <div class="col-lg-6">
+            <img src="https://ibixion.com/wp-content/uploads/2020/04/Hacker-II-1.jpg" class="img-fluid">
+          </div>
+          <div class="col-lg-5">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sed perspiciatis nobis ipsa quas, fugit esse! Ipsum ea ipsa explicabo quo assumenda doloribus blanditiis hic quos, dolorem nulla, autem incidunt, sapiente facere eveniet laudantium. Reprehenderit illum cumque consequuntur, cum dolor repellat accusantium ut nemo eius in perferendis doloribus? Amet suscipit alias obcaecati aut explicabo. Eum nesciunt quisquam, similique totam possimus sunt rerum sed ratione? Exercitationem delectus culpa fugiat asperiores quasi repudiandae facere perferendis autem non? Consequatur nostrum assumenda pariatur eum magnam id distinctio corporis odio quia debitis. Qui totam adipisci magnam voluptate tenetur, fugiat velit neque rem, porro molestias doloribus.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end why  -->
+
+    <!-- card -->
+
+    <section class="page-card">
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-12 float-end text-end">
+            <h1 class="service-tittle-info">Services.</h1>
+            <h3 class="service-mini-info">OUR SERVICES FOR CLIENTS</h3>
+          </div>
+        </div>
+      <div class="row test1">
+          <!-- section about us  -->
+        <div class="col-lg-3 test">
+          <div class="card1">
+                <a href="#" class="card__link">
+              <!-- Media -->
+              <div class="row">
+                <div class="card__media">
+                  <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
+                </div>
+              </div>
+              <!-- Header -->
+              <div class="row">
+                <div class="card__header">
+                  <h3 class="card__header-title">Title of Card</h3>
+                  <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div class="row">
+                  <div class="card__header-icon">
+                    <svg viewbox="0 0 28 25">
+                      <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
+                    </svg>
+                  </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col-lg-3 test">
+          <div class="card1">
+                <a href="#" class="card__link">
+              <!-- Media -->
+              <div class="row">
+                <div class="card__media">
+                  <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
+                </div>
+              </div>
+              <!-- Header -->
+              <div class="row">
+                <div class="card__header">
+                  <h3 class="card__header-title">Title of Card</h3>
+                  <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div class="row">
+                  <div class="card__header-icon">
+                    <svg viewbox="0 0 28 25">
+                      <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
+                    </svg>
+                  </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col-lg-3 test">
+          <div class="card1">
+                <a href="#" class="card__link">
+              <!-- Media -->
+              <div class="row">
+                <div class="card__media">
+                  <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
+                </div>
+              </div>
+              <!-- Header -->
+              <div class="row">
+                <div class="card__header">
+                  <h3 class="card__header-title">Title of Card</h3>
+                  <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div class="row">
+                  <div class="card__header-icon">
+                    <svg viewbox="0 0 28 25">
+                      <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
+                    </svg>
+                  </div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 test">
+          <div class="card1">
+                <a href="#" class="card__link">
+              <!-- Media -->
+              <div class="row">
+                <div class="card__media">
+                  <img src="./assets/rockpaperstrategy-1600.jpg" class="img-fluid">
+                </div>
+              </div>
+              <!-- Header -->
+              <div class="row">
+                <div class="card__header">
+                  <h3 class="card__header-title">Title of Card</h3>
+                  <p class="card__header-meta">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div class="row">
+                  <div class="card__header-icon">
+                    <svg viewbox="0 0 28 25">
+                      <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z"/>
+                    </svg>
+                  </div>
+              </div>
+            </a>
+          </div>
+        </div>
+          
+          
+      </div>
+  </div>
+</section>
+
+
+    <!-- end card -->
+    
+    <!-- section about us  -->
+    <section class="page-about">
+      <div class="container">
+        <div class="row about">
+          <h1>About Us</h1>
+        </div>
+        <div class="row about-us">
+          <div class="col-lg-6">
+            <img src="https://ibixion.com/wp-content/uploads/2020/04/Hacker-II-1.jpg" class="img-fluid">
+          </div>
+          <div class="col-lg-5">
+            <h3>You work like at home</h3>
+            <p>Bekerja dengan suasana hati yang lebih asik dan mempelajari hal baru setiap harinya.</p>
+            <a href="#" class="btn btn-primary tombol">Gallery</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end about us  -->
+    
+    <!-- footer -->
+
+    <section class="page-footer">
+      <div class="container-fluid">
+        <div class="row footer border-bottom">
+          <div class="col-md-3">
+            <h4>About Us</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime molestias aliquam enim odio ut beatae doloremque modi quo quidem distinctio, suscipit perspiciatis magnam omnis dolorem ex, nisi ad qui ipsa.</p>
+          </div>
+          <div class="col-md-3">
+            <div class="row">
+              <div class="col-12">
+                <h4>Contact US</h4>
+                <ul>
+                  <li>081930812</li>
+                  <li>kontol@gmail.com</li>
+                  <li>di cerita senja</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bawah">
+          <p class="text-left text-muted">&copy; 2022, Inc. All Right Reserved</p>
+      </div>
+    </section>
+
+
+
+    <!-- footer -->
+    
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
